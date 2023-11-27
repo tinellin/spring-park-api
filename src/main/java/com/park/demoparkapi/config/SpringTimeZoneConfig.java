39c2197a3b85e494a2f570a614241a5bafcd1066
tinellin <enzo.tinelli@unesp.br>
2023-11-26 21:44:13 -0300

    add: user entity

---
+++ src/main/java/com/park/demoparkapi/config/SpringTimeZoneConfig.java
@@ -0,0 +1,15 @@
+package com.park.demoparkapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimeZoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
---
+++ src/main/java/com/park/demoparkapi/entity/User.java
@@ -0,0 +1,70 @@
+package com.park.demoparkapi.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter() @Setter @NoArgsConstructor()
+@Entity()
+@Table(name = "users")
+/* Implementamos Serializable, pois é uma boa prática */
+public class User implements Serializable {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    /* Representa o sistema de auditoria */
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public enum Role {
+        ROLE_ADMIN,
+        ROLE_USER
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
